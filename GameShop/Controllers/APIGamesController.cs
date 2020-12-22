f6daf1193d470d7b1071c506b77f2dae7731b8fc
Daniil <dr-0901@mail.ru>
2020-12-22 17:58:06 +0300

    Добавление API-controller'а

---
+++ GameShop/Controllers/APIGamesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GameShop;
+using GameShop.Models;
+
+namespace GameShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIGamesController : ControllerBase
+    {
+        private readonly AppDBContent _context;
+
+        public APIGamesController(AppDBContent context)
+        {
+            _context = context;
+        }
+
+        // GET: api/APIGames
+        [HttpGet]
+        public IEnumerable<Game> GetGame()
+        {
+            return _context.Game;
+        }
+
+        // GET: api/APIGames/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Game>> GetGame(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var game = await _context.Game.FindAsync(id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(game);
+        }
+
+        // PUT: api/APIGames/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGame(int id, Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != game.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(game).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/APIGames
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Game>> PostGame(Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Game.Add(game);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGame", new { id = game.id }, game);
+        }
+
+        // DELETE: api/APIGames/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Game>> DeleteGame(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var game = await _context.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            _context.Game.Remove(game);
+            await _context.SaveChangesAsync();
+
+            return game;
+        }
+
+        private bool GameExists(int id)
+        {
+            return _context.Game.Any(e => e.id == id);
+        }
+    }
+}
